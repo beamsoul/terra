3324b7c89a6f80e9e70a8837bfa488f54c05c0da
beamsoul <nurzhanasamidinova@gmail.com>
2019-11-06 15:32:54 -0800

    pushing

---
+++ modules/build.lauch.tf
@@ -0,0 +1,2 @@
+#!bin/bash
+AMI_ID= 
--- modules/module.tf
+++
@@ -1,8 +0,0 @@
-module "consul" {
-    source = "https://github.com/beamsoul/terraform-consul-module.git"
-    key_name = "${aws_key_pair.mykey.key_name}"
-    key_path = "${var.PATH_TO_PRIVATE_KEY}"
-}
-output "consul-output" {
-    value = "${module.consul.server_address}"
-}
